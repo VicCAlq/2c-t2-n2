631f884fe5a6e07438dd74a01e8a61c184b44179
Isaac-Andrade-Santos-NAVE <isaac.andrade@nave.org.br>
2026-08-07 10:14:36 -0300

    Atualizou

--- src/classes/fontesNoticias.js
+++ src/classes/fontesNoticias.js
@@ -1,3 +1,8 @@
 class FonteNoticias {
-
+    constructor(nome, endereco, descricao, categoria) {
+        this.nome = nome
+        this.endereco = endereco
+        this.descricao = descricao
+        this.categoria = categoria
+    }
 }
