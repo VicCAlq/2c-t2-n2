d6c6f937c9427fdde1ceabb4bf0a1f5b1a3ad6a0
gabriel-fernando-nave <gabriel.fernando@nave.org.br>
2026-08-07 11:08:46 -0300

    Add InputTexto component for text input

---
+++ src/components/InputTexto.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+export default function InputTexto() {
+  const [textoInserido, setTextoInserido] = useState("")
+  return(
+    <div>
+      <input
+        type="text"
+        value={textoInserido}
+        onChange={(e) => setTextoInserido(e.target.value)}
+        placeholder="Digite aqui o novo texto"
+      />
+
+    </div>
+  )
+}
