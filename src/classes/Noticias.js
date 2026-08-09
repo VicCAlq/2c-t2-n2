90c991ec234d0c30f32f96646a82143c522e166c
Andrey Vieira <andreyantonio2020@gmail.com>
2026-08-08 21:15:03 -0300

    algo

--- src/classes/Noticias.js
+++ src/classes/Noticias.js
@@ -1,20 +1,20 @@
-import { useState } from 'react';
-
 class FontesNoticias {
     constructor(nome, endereco, descricao, categoria) {
         this.nome = nome;
-        this.endereco;
-        this.descricao;
-        this.categoria;
+        this.endereco = endereco;
+        this.descricao = descricao;
+        this.categoria = categoria;
     }
 }
 
 class Noticias {
     constructor(nome, endereco, descricao, dataDePublicacao,  categoria) {
         this.nome = nome;
-        this.endereco;
-        this.descricao;
-        this.dataDePublicacao;
-        this.categoria;
+        this.endereco = endereco;
+        this.descricao = descricao;
+        this.dataDePublicacao = dataDePublicacao;
+        this.categoria = categoria;
     }
 }
+
+export { FontesNoticias, Noticias };
