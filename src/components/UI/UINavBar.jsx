086aab0b33f5059cec47dc183d9519e0beac65e1
mateusnave <mateus.antonio@nave.org.br>
2026-08-10 11:18:10 -0300

    add: UI components

---
+++ src/components/UI/UINavBar.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function UINavBar() {
+
+    return (
+        <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="assets\logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            React Bootstrap
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    );
+}
+
+export default UINavBar;
