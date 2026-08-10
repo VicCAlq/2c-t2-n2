3dba086587b22c9bb8e7afc853ad2b118691c3f8
Ryan-Nave2026 <ryan.nave2026@gmail.com>
2026-08-10 12:00:26 -0300

    fix

--- src/components/ClassesCriadas.js
+++ src/components/ClassesCriadas.js
@@ -1,1 +1,20 @@
 
+export class FonteNoticia {
+  constructor(nome, endereco, descricao = '', categoria = 'Geral') {
+    this.nome = nome;
+    this.endereco = endereco;
+    this.descricao = descricao;
+    this.categoria = categoria;
+  }
+}
+
+export class Noticia {
+    constructor(nome, nomeFonte, link, descricao, dataPublicacao, categorias){
+        this.nome = nome;
+        this.nomeFonte = nomeFonte;
+        this.link = link;
+        this.descricao = descricao;
+        this.dataPublicacao = dataPublicacao;
+        this.categorias = categorias;
+    }
+}
