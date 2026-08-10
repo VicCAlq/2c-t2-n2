0b3eb160a9d60f1bc0a625c9ccea1c77ddd33bf0
Ryan-Nave2026 <ryan.nave2026@gmail.com>
2026-08-10 11:59:43 -0300

    Add TabelaNoticias component to display news table

--- src/components/TabelaNoticias.jsx
+++ src/components/TabelaNoticias.jsx
@@ -1,1 +1,31 @@
 
+export default function TabelaNoticias({ noticias }) {
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Fonte</th>
+                    <th>Descrição</th>
+                    <th>Data de publicação</th>
+                    <th>Categorias</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {noticias.map((noticia) => (
+                    <tr key={noticia.id ?? noticia.link}>
+                        <td>{noticia.nome}</td>
+                        <td>{noticia.nomeFonte}</td>
+                        <td>{noticia.descricao}</td>
+                        <td>{noticia.dataPublicacao}</td>
+                        <td>
+                            {noticia.categorias?.join(", ")}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
