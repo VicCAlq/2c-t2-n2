b32c7e4cb7a31e22a06d96b5f57223b0ab7a1535
mateusnave <mateus.antonio@nave.org.br>
2026-08-15 19:03:27 -0300

    update: change table, create utils, create input

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import Exemplo from "./components/Exemplo";
+import Exemplo from "./components/UI/UIModal";
+import UINavBar from "./components/UI/UINavBar";
+import UITable from "./components/UI/UITable";
+import UIInput from "./components/UI/UIInput";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const styles = {
   container: {
-    flex: 1,
     backgroundColor: "#eec",
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    color: "#101015"
   }
 };
 
 export default function App() {
   return (
     <div style={styles.container}>
-      <h1>Crie seu aplicativo React aqui</h1>
-      <Exemplo>Componente de exemplo aqui</Exemplo>
+      <UINavBar> </UINavBar>
+      <UIInput> </UIInput>
+      <UITable> </UITable>
     </div>
   );
 }
---
+++ src/components/UI/UINavBar.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function UINavBar() {
+
+    return (
+        <Navbar bg='dark' data-bs-theme="dark" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt="G8 Logo"
+              src="assets\logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            G8
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    );
+}
+
+export default UINavBar;
