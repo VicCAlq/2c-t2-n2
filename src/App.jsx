d62ec9d3e22edba4b68cfbe5c476a4f2563f0cd0
Ryan-Nave2026 <ryan.nave2026@gmail.com>
2026-08-19 17:32:29 -0300

    Update App component with state and improved JSXfix
    Refactor App component to use state for noticias and update rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useEffect, useState } from "react";
 import { listarNoticias } from "./components/database";
 import Exemplo from "./components/Exemplo";
 import TabelaNoticias from "./components/TabelaNoticias";
+import "./App.css";
 
 const styles = {
   container: {
     flex: 1,
     backgroundColor: "#eec",
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
+
   divTitulo: {
     backgroundColor: "rgba(80, 80, 13, 1)",
     display: "flex",
@@ -18,16 +21,32 @@ const styles = {
     marginLeft: "250px",
     width: "900px",
     borderRadius: "25px",
-    color: "#e8e8f0ff"
+    color: "#e8e8f0ff",
   },
 };
 
 export default function App() {
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(() => {
+    listarNoticias().then((dados) => {
+      setNoticias(dados);
+    });
+  }, []);
+
   return (
-    <div style={styles.container}>
-      <h1 style={styles.divTitulo}>Notícias Do Amanhã!</h1>
-      <Exemplo>Veja Notícias Novas!</Exemplo>
-      <TabelaNoticias noticias={listarNoticias()}/>
+  <div className="app">
+    <h1 className="titulo">
+      Notícias Do Amanhã!
+    </h1>
+
+    <div className="importador">
+      <Exemplo>
+        Veja Notícias Novas!
+      </Exemplo>
     </div>
+
+    <TabelaNoticias noticias={noticias} />
+  </div>
   );
 }
