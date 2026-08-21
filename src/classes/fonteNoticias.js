1a7a8d09fccf14c1d6b4c9809ead5cc1b88962f7
Futuro-program <isaac.andrade@nave.org.br>
2026-08-21 11:40:23 -0300

    Adicionou o resto.

--- src/classes/fonteNoticias.js
+++
@@ -1,8 +0,0 @@
-export default class FonteNoticias {
-    constructor(nome, link, descricao, categoria) {
-        this.nome = nome
-        this.descricao = descricao
-        this.link = link
-        this.categoria = categoria
-    }
-}
--- src/classes/noticia.js
+++
@@ -1,9 +0,0 @@
-export default class Noticia {
-    constructor(titulo, link, descricao, dataDePublicacao, categoria) {
-        this.titulo = titulo
-        this.descricao = descricao
-        this.link = link
-        this.dataDePublicacao = dataDePublicacao
-        this.categoria = categoria
-    }
-}
